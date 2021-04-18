236e7c9c9ae97f23b982189b298e9ad9bfd54a52
yogaaaaaaa <yogaadhi@icloud.com>
2021-04-18 22:40:25 +0700

    initial commit for yoga branch

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   POST api/users
+// @desc    Register User
+// @access  Public
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("User Route");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const app = express();
 //connect Database
 connectDB();
 
+//init middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
 app.get("/", (req, res) => res.send("API Running"));
 
 // define routes
-app.use("/api/users", require("./api/users.js"));
-app.use("/api/auth", require("./api/auth.js"));
-app.use("/api/profile", require("./api/profile.js"));
-app.use("/api/posts", require("./api/posts.js"));
+app.use("/api/users", require("./routes/api/users.js"));
+app.use("/api/auth", require("./routes/api/auth.js"));
+app.use("/api/profile", require("./routes/api/profile.js"));
+app.use("/api/posts", require("./routes/api/posts.js"));
 
 const PORT = process.env.PORT || 5000;
 
