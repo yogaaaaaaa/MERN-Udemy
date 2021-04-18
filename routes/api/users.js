556591e21c84c4266fb7989d9c39defc40b2e40a
yogaaaaaaa <yogaadhi@icloud.com>
2021-04-18 23:03:37 +0700

    adding validation for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User Route");
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "please include a valid Email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
