a965a8cf05f9ed0d7f0127ed018122f21fdcc8c7
yogaaaaaaa <yogaadhi@icloud.com>
2021-04-18 22:24:09 +0700

    adding routes

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//connect Database
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+// define routes
+app.use("/api/users", require("./api/users.js"));
+app.use("/api/auth", require("./api/auth.js"));
+app.use("/api/profile", require("./api/profile.js"));
+app.use("/api/posts", require("./api/posts.js"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server up adn running ${PORT}`));
+app.listen(PORT, () => console.log(`Server up and running ${PORT}`));
