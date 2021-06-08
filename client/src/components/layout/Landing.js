7b39f6ad252a814ea10305833fe48f7143b37672
yogaaaaaaa <yogaadhi@icloud.com>
2021-06-08 20:17:32 +0700

    adding create profile

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import propTypes from "prop-types";
 
+const Landing = ({ isAuthenticated }) => {
 
-const Landing = () => {
+  if(isAuthenticated){
+   return <Redirect to='/dashboard'/>
+  }
+
   return (
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
           <h1 className="x-large">Developer Social-Hub</h1>
           <p className="lead">
-            Menghubungkan para developer! Buat profil/portofolio, bagikan postingan dan cari bantuan dari developer lain !
+            Menghubungkan para developer! Buat profil/portofolio, bagikan
+            postingan dan cari bantuan dari developer lain !
           </p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">
@@ -25,4 +32,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: propTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,15 +4,20 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
 
-
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <a onClick={logout} href='#!'>
-         <i className='fas fa-sign-out-alt'></i>{' '}
-         <span className="hide-sm">Logout</span> 
-          </a>
+        <Link to="/dashboard">
+          <i className="fas fa-user"></i>{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
       </li>
     </ul>
   );
@@ -38,7 +43,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
           <i className="fas fa-code"></i> Dev-Social
         </Link>
       </h1>
-      {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>) }
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
